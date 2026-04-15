22901762cc53ad9f904cbcf12aea334f6a92099e
KingVishal703 <vishal21wankhede@gmail.com>
2026-04-15 15:50:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,47 +1,18 @@
 const CONFIG = {
-
-  // 💰 Earnings Control
   earnings: {
     dailyBonus: 5,
     adReward: 2,
-    gameWin: 1,
+    gameReward: 1,
     spinMin: 1,
     spinMax: 10,
-    scratchMin: 1,
-    scratchMax: 5
+    referReward: 10
   },
 
-  // 💳 Withdraw
   withdraw: {
-    min: 50,
-    max: 1000
+    min: 50
   },
 
-  // 👥 Referral
-  referral: {
-    reward: 5
-  },
-
-  // 📺 Ads Control
   ads: {
-    adsgram: {
-      enable: true,
-      blockId: "27402"
-    },
-    monetag: {
-      enable: false
-    },
-    direct: {
-      enable: false,
-      url: ""
-    }
-  },
-
-  // 🎮 Games Control
-  games: {
-    tapGame: true,
-    snakeGame: true,
-    memoryGame: true
+    enable: true
   }
-
 };
